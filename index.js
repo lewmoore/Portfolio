e488cee7a26d55e65de983c84442a5b818eecd98
Lewis Moore <lewis.moore2707@gmail.com>
2018-04-26 16:24:14 +0100

    Add app launching on localhost:3000

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
+var port = process.env.PORT || 3000;
 app.set("view engine", "ejs");
 app.set("views", "views")
 
 app.get('/', function(req, res) {
   res.render('homepage')
 })
+
+app.listen(port);
+console.log("Youre now on localhost " + port);
 
 module.exports = app
